fea072f96d1ed81e7b6b679a11c49a6269f2ab3d
Praveenkarmegam <lovelypraveendp@gmail.com>
2024-10-20 10:08:16 +0530

    updated

--- password-reset-flow/backend/index.js
+++ password-reset-flow/backend/index.js
@@ -5,9 +5,9 @@ const userRoutes = require("./routes/userRoutes");
 const cors = require('cors');
 
 const app = express();
-app.use(express.json());
 
 app.use(cors());
+app.use(bodyParser.json());
 
 // Routes
 app.use("/api", userRoutes);
